ec1d5b2bbd64183e3b7b35e4368107407b8eee88
Ismail-azam <Ismail.appcrates@gmail.com>
2017-05-03 11:47:42 +0500

    Green-tech local backuup
    Green-tech local backuup

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/green_tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/green_tech/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/VertrigoServ/www/green_tech/catalog/');
+define('DIR_SYSTEM', 'E:/VertrigoServ/www/green_tech/system/');
+define('DIR_IMAGE', 'E:/VertrigoServ/www/green_tech/image/');
+define('DIR_LANGUAGE', 'E:/VertrigoServ/www/green_tech/catalog/language/');
+define('DIR_TEMPLATE', 'E:/VertrigoServ/www/green_tech/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/VertrigoServ/www/green_tech/system/config/');
+define('DIR_CACHE', 'E:/VertrigoServ/www/green_tech/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/VertrigoServ/www/green_tech/system/storage/download/');
+define('DIR_LOGS', 'E:/VertrigoServ/www/green_tech/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/VertrigoServ/www/green_tech/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/VertrigoServ/www/green_tech/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vertrigo');
+define('DB_DATABASE', 'test_greentech');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ oldconfig.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.verfix.com/greentech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.verfix.com/greentech/');
+
+// DIR
+define('DIR_APPLICATION', '/home/verfix/public_html/greentech/catalog/');
+define('DIR_SYSTEM', '/home/verfix/public_html/greentech/system/');
+define('DIR_IMAGE', '/home/verfix/public_html/greentech/image/');
+define('DIR_LANGUAGE', '/home/verfix/public_html/greentech/catalog/language/');
+define('DIR_TEMPLATE', '/home/verfix/public_html/greentech/catalog/view/theme/');
+define('DIR_CONFIG', '/home/verfix/public_html/greentech/system/config/');
+define('DIR_CACHE', '/home/verfix/public_html/greentech/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/verfix/public_html/greentech/system/storage/download/');
+define('DIR_LOGS', '/home/verfix/public_html/greentech/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/verfix/public_html/greentech/system/storage/modification/');
+define('DIR_UPLOAD', '/home/verfix/public_html/greentech/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'verfix_greendemouser');
+define('DB_PASSWORD', '@D6pMBKR+Oa[');
+define('DB_DATABASE', 'verfix_greendemo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
